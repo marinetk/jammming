ba097d458c517cbf1590ad1e3393a06a36948daf
marinetk <marine.stoll@gmail.com>
2019-03-27 16:46:32 +0100

    bug fixxed

--- src/Component/SearchResults/SearchResults.js
+++ src/Component/SearchResults/SearchResults.js
@@ -6,8 +6,7 @@ import {TrackList} from '../TrackList/TrackList.js'
 export class SearchResults extends React.Component {
 
     render() {
-        return
-        <div className = "SearchResults" >
+        return <div className = "SearchResults" >
             <h2 > Results < /h2>
             <TrackList
               tracks = {this.props.searchResults}
